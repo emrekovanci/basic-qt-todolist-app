435aa1c6f08fde99397ca9035f4d32226b3757aa
Emre Kovanci <kovanciemre@gmail.com>
2022-12-03 13:03:15 +0300

    up

---
+++ ui_MainWindow.h
@@ -0,0 +1,111 @@
+/********************************************************************************
+** Form generated from reading UI file 'MainWindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.4.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QProgressBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *toolBarLayout;
+    QProgressBar *progressBar;
+    QSpacerItem *horizontalSpacer;
+    QPushButton *addTaskButton;
+    QPushButton *clearAllButton;
+    QVBoxLayout *tasksLayout;
+    QSpacerItem *verticalSpacer;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(326, 175);
+        MainWindow->setAutoFillBackground(true);
+        MainWindow->setStyleSheet(QString::fromUtf8(""));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        verticalLayout = new QVBoxLayout(centralwidget);
+        verticalLayout->setObjectName("verticalLayout");
+        toolBarLayout = new QHBoxLayout();
+        toolBarLayout->setObjectName("toolBarLayout");
+        progressBar = new QProgressBar(centralwidget);
+        progressBar->setObjectName("progressBar");
+        QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(progressBar->sizePolicy().hasHeightForWidth());
+        progressBar->setSizePolicy(sizePolicy);
+        progressBar->setValue(24);
+
+        toolBarLayout->addWidget(progressBar);
+
+        horizontalSpacer = new QSpacerItem(20, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        toolBarLayout->addItem(horizontalSpacer);
+
+        addTaskButton = new QPushButton(centralwidget);
+        addTaskButton->setObjectName("addTaskButton");
+        addTaskButton->setAutoFillBackground(true);
+        addTaskButton->setStyleSheet(QString::fromUtf8(""));
+
+        toolBarLayout->addWidget(addTaskButton);
+
+        clearAllButton = new QPushButton(centralwidget);
+        clearAllButton->setObjectName("clearAllButton");
+
+        toolBarLayout->addWidget(clearAllButton);
+
+
+        verticalLayout->addLayout(toolBarLayout);
+
+        tasksLayout = new QVBoxLayout();
+        tasksLayout->setObjectName("tasksLayout");
+
+        verticalLayout->addLayout(tasksLayout);
+
+        verticalSpacer = new QSpacerItem(20, 120, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout->addItem(verticalSpacer);
+
+        MainWindow->setCentralWidget(centralwidget);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "toDO", nullptr));
+        addTaskButton->setText(QCoreApplication::translate("MainWindow", "Add Task", nullptr));
+        clearAllButton->setText(QCoreApplication::translate("MainWindow", "Clear All", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
---
+++ ui_Task.h
@@ -0,0 +1,84 @@
+/********************************************************************************
+** Form generated from reading UI file 'Task.ui'
+**
+** Created by: Qt User Interface Compiler version 6.4.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_TASK_H
+#define UI_TASK_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Task
+{
+public:
+    QHBoxLayout *horizontalLayout;
+    QCheckBox *checkBox;
+    QSpacerItem *horizontalSpacer;
+    QPushButton *editButton;
+    QPushButton *removeButton;
+
+    void setupUi(QWidget *Task)
+    {
+        if (Task->objectName().isEmpty())
+            Task->setObjectName("Task");
+        Task->resize(400, 41);
+        horizontalLayout = new QHBoxLayout(Task);
+        horizontalLayout->setObjectName("horizontalLayout");
+        checkBox = new QCheckBox(Task);
+        checkBox->setObjectName("checkBox");
+        QSizePolicy sizePolicy(QSizePolicy::Minimum, QSizePolicy::Fixed);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(checkBox->sizePolicy().hasHeightForWidth());
+        checkBox->setSizePolicy(sizePolicy);
+
+        horizontalLayout->addWidget(checkBox);
+
+        horizontalSpacer = new QSpacerItem(138, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer);
+
+        editButton = new QPushButton(Task);
+        editButton->setObjectName("editButton");
+
+        horizontalLayout->addWidget(editButton);
+
+        removeButton = new QPushButton(Task);
+        removeButton->setObjectName("removeButton");
+
+        horizontalLayout->addWidget(removeButton);
+
+
+        retranslateUi(Task);
+
+        QMetaObject::connectSlotsByName(Task);
+    } // setupUi
+
+    void retranslateUi(QWidget *Task)
+    {
+        Task->setWindowTitle(QCoreApplication::translate("Task", "Form", nullptr));
+        checkBox->setText(QCoreApplication::translate("Task", "Buy Milk", nullptr));
+        editButton->setText(QCoreApplication::translate("Task", "Edit", nullptr));
+        removeButton->setText(QCoreApplication::translate("Task", "Remove", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Task: public Ui_Task {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_TASK_H
