aab711c0ea6594872c4d80f0d9e1b3d442a6f22a
Emre Kovanci <kovanciemre@gmail.com>
2022-12-03 13:37:30 +0300

    up

--- Task.cpp
+++ Task.cpp
@@ -5,60 +5,78 @@
 #include <QString>
 #include <QInputDialog>
 
-Task::Task(const QString& name, QWidget* parent) :
-    QWidget(parent),
-    ui(new Ui::Task) {
+Task::Task(const QString& name, QWidget* parent) : QWidget(parent), ui(new Ui::Task)
+{
     ui->setupUi(this);
     setName(name);
     connect(ui->editButton, &QPushButton::clicked, this, &Task::rename);
-    connect(ui->removeButton, &QPushButton::clicked, [this, name] {
+    connect(ui->removeButton, &QPushButton::clicked, [this, name]
+    {
         qDebug() << "Trying to remove " << name;
         this->emit removed(this);
     });
     connect(ui->checkBox, &QCheckBox::toggled, this, &Task::checked);
 }
 
-Task::~Task() {
+Task::~Task()
+{
     delete ui;
 }
 
-void Task::setName(const QString& name) {
+void Task::setName(const QString& name)
+{
     ui->checkBox->setText(name);
 }
 
-QString Task::name() const {
+QString Task::name() const
+{
     return ui->checkBox->text();
 }
 
-bool Task::isCompleted() const {
+bool Task::isCompleted() const
+{
     return ui->checkBox->isChecked();
 }
 
-void Task::setChecked(bool checked) {
+void Task::setChecked(bool checked)
+{
     QFont font(ui->checkBox->font());
     font.setStrikeOut(checked);
     ui->checkBox->setFont(font);
     emit statusChanged(this);
 }
 
-int Task::getTaskId() const {
+int Task::getTaskId() const
+{
     return taskId;
 }
 
-void Task::setTaskId(int id) {
+void Task::setTaskId(int id)
+{
     taskId = id;
 }
 
-void Task::rename() {
+void Task::rename()
+{
     bool ok{ false };
-    QString value = QInputDialog::getText(this, tr("Edit Task"), tr("Task Name"), QLineEdit::Normal, this->name(), &ok);
 
-    if (ok && !value.isEmpty()) {
+    QString value = QInputDialog::getText(
+        this,
+        tr("Edit Task"),
+        tr("Task Name"),
+        QLineEdit::Normal,
+        this->name(),
+        &ok
+    );
+
+    if (ok && !value.isEmpty())
+    {
         setName(value);
     }
 }
 
-void Task::checked(bool checked) {
+void Task::checked(bool checked)
+{
     QFont font(ui->checkBox->font());
     font.setStrikeOut(checked);
     ui->checkBox->setFont(font);
